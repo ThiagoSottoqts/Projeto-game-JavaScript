9f5de556090ca5ee42d498e0c4c9221d7f6dfe71
Thiago Sotto <thiago.sotto@hotmail.com>
2021-08-11 14:32:38 -0300

    Inclusão do Estilo Css

--- App2/jogo.js
+++ App2/jogo.js
@@ -1,5 +1,5 @@
 var altura = 0
-var larguta = 0
+var largura = 0
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -9,3 +9,25 @@ function ajustaTamanhoPalcoJogo(){
 }
 
 ajustaTamanhoPalcoJogo()
+
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX, posicaoY)
+
+//criar o elemento HTML
+
+function posicaoRandomica(){
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}
