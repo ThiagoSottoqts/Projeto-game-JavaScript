999e0868c7aff786abcf995d7da30d26f97a9c44
Thiago Sotto <thiago.sotto@hotmail.com>
2021-08-11 09:05:25 -0300

    Inserção de código Javascript

---
+++ App2/jogo.js
@@ -0,0 +1,11 @@
+var altura = 0
+var larguta = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
